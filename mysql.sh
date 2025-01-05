6dc42a06843b167ef44a12f21de2b0c5ec78f9ef
shalinic26 <shalinichintala@gmail.com>
2025-01-05 14:51:38 +0530

    Connecting to mysql validation expense shell script

--- mysql.sh
+++ mysql.sh
@@ -43,5 +43,13 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "Setting Root Password"
+mysql -h mysql.expenseslist.online -u root -pExpenseApp@1 -e 'show databases' &>>$LOG_FILE_NAME
+
+if [$? -ne 0 ]
+then
+    echo "MySQL Root Password not setup" &>>$LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting Root Password"
+else
+    echo -e "MySQL Root Passwoord already setup.....$Y SKIPPING $N"
+fi
